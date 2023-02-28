0b12d1c118e40f8cadf99c795075bf8bae1a007b
ömer turan <126421999+omerturan-developer@users.noreply.github.com>
2023-03-01 01:11:47 +0300

    kredi hesaplama formu
    javascript

---
+++ kredi hesaplama formu/main.js
@@ -0,0 +1,28 @@
+function krediHesapla(){
+    var cekilecekTutar,vadeSayisi;
+    var aylikTaksit,odenecekToplamTutar;
+
+    cekilecekTutar = document.getElementById("txtKrediTutari").value;
+    var liste = document.getElementById("listeVade");
+    vadeSayisi = liste.options[liste.selectedIndex].value;
+
+    if(vadeSayisi==12){
+        odenecekToplamTutar = cekilecekTutar*1.1;
+    }
+    else if(vadeSayisi==24){
+        odenecekToplamTutar = cekilecekTutar*1.2;
+    }
+    else if(vadeSayisi==36){
+        odenecekToplamTutar = cekilecekTutar*1.3;
+    }
+    else if(vadeSayisi==48){
+        odenecekToplamTutar = cekilecekTutar*1.4;
+    }
+
+
+    aylikTaksit = odenecekToplamTutar/vadeSayisi;
+
+    document.querySelector("#sonuc").innerHTML="Geri Ödeme Toplamı : "+odenecekToplamTutar+"<br>"+
+    "Aylık Taksit Tutarınız : "+aylikTaksit.toFixed(2);
+
+}
